eda54d02c9f751a0762d33ba9d3ab35d43ef90cf
Massimiliano Mirra <bard@hyperstruct.net>
2023-07-07 14:36:08 +0100

    fix: typo in request body

--- src/index.ts
+++ src/index.ts
@@ -459,7 +459,7 @@ export class Indexer<T extends Storage> {
       const url = this.provider.connection.url;
 
       const body = {
-        jsonprc: "2.0",
+        jsonrpc: "2.0",
         id: "1",
         method: "eth_getLogs",
         params: [
